5b817879ece0852583561286187ca1df3db7a4b5
Gargeeshah <gargeeshah@gmail.com>
2021-11-23 16:21:49 +0530

    CRUD operations with Unit testing

--- Course_catalog/src/main/java/com/spring/crudoperations/dao/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.crudoperations.dao;
-
-import com.spring.crudoperations.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, String> {
-}
--- Course_catalog/src/main/java/com/spring/crudoperations/dao/TopicRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.crudoperations.dao;
-
-
-import com.spring.crudoperations.model.Topic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TopicRepository extends JpaRepository<Topic, String> {
-
-}
